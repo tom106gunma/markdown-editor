3c077e041ad10fdc07872595ac8395770cc88b18
tom106gunma <>
2023-02-16 23:12:38 +0900

    テキスト履歴を復元できるように変更

--- src/index.tsx
+++ src/index.tsx
@@ -10,26 +10,39 @@ import {
 } from 'react-router-dom'
 import { Editor } from './pages/editor'
 import { History } from './pages/history'
+import { useStateWithStorage } from './hooks/use_state_with_storage'
 
 const GlobalStyle = createGlobalStyle`
   body * {
     box-sizing: border-box;
   }
 `
+const StorageKey = '/editor:text'
 
-const Main = (
+const Main: React.FC = () => {
+  const [ text, setText ] = useStateWithStorage('', StorageKey)
+
+  return (
   <>
     <GlobalStyle />
     <Router>
-      <Route exact path="/editor">
-        <Editor />
-      </Route>
-      <Route exact path="/history">
-        <History />
-      </Route>
+      <Switch>
+        <Route exact path="/editor">
+          <Editor
+            text={text}
+            setText={setText}
+          />
+        </Route>
+        <Route exact path="/history">
+          <History
+            setText={setText}
+          />
+        </Route>
+      </Switch>
       <Redirect to="/editor" path="*" />
     </Router>
   </>
-)
+  )
+}
 
-render(Main, document.getElementById('app'))
+render(<Main />, document.getElementById('app'))
